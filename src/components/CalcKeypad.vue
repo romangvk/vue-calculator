fb18ef6241dfd1b285c58b65ca13242f517e59e3
Roman Kasparian <roman.kasparian@gmail.com>
2021-09-02 13:41:42 -0400

    on click event emissions

--- src/components/CalcKeypad.vue
+++ src/components/CalcKeypad.vue
@@ -1,9 +1,17 @@
 <template>
   <div className="container">
-    <button type="button" class="key1">C</button>
-    <button type="button" class="key2">+</button>
-    <button type="button" class="key3">0</button>
-    <button type="button" class="key4">=</button>
+    <button type="button" class="key1" v-on:click="$emit('keypress', 'C')">
+      C
+    </button>
+    <button type="button" class="key2" v-on:click="$emit('keypress', '+')">
+      +
+    </button>
+    <button type="button" class="key3" v-on:click="$emit('keypress', '0')">
+      0
+    </button>
+    <button type="button" class="key4" v-on:click="$emit('keypress', '=')">
+      =
+    </button>
     <button
       v-for="(k, i) of [
         '/',
@@ -22,6 +30,7 @@
       ]"
       :key="i"
       type="button"
+      v-on:click="$emit('keypress', k)"
     >
       {{ k }}
     </button>
