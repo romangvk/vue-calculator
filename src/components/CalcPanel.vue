ede8e48e634bb20680cca211f37a068259640d5f
Roman Kasparian <roman.kasparian@gmail.com>
2021-09-02 13:44:23 -0400

    handling emitted events

--- src/components/CalcPanel.vue
+++ src/components/CalcPanel.vue
@@ -1,7 +1,7 @@
 <template>
   <span class="panel">
-    <CalcOutput></CalcOutput>
-    <CalcKeypad></CalcKeypad>
+    <CalcOutput text="text"></CalcOutput>
+    <CalcKeypad @keypress="keypress"></CalcKeypad>
   </span>
 </template>
 
@@ -9,11 +9,62 @@
 import CalcOutput from "./CalcOutput.vue";
 import CalcKeypad from "./CalcKeypad.vue";
 
+import evaluate from "../lib/evaluate";
+
 export default {
-  name: "Panel",
+  name: "CalcPanel",
   components: {
     CalcOutput,
     CalcKeypad,
+  },
+  data: function() {
+    {
+      return { text: "" };
+    }
+  },
+  methods: {
+    keypress: function(key) {
+      let prev = this.text;
+      if (
+        this.text === "NaN" ||
+        this.text === "Infinity" ||
+        this.text === "-Infinity" ||
+        this.text === "Err: Division by 0"
+      ) {
+        prev = "";
+      }
+      switch (key) {
+        case "C":
+          this.text = "";
+          break;
+        case "/":
+        case "*":
+        case "+":
+          // only add an operator after a number
+          if (/\d$/.test(prev)) this.text = prev + key;
+          break;
+        case "-":
+          // preceded by + change to minus
+          if (/\+$/.test(prev)) this.text = prev.replace(/.$/, "-");
+          // preceded by - change to plus
+          else if (/\d-$/.test(prev)) this.text = prev.replace(/.$/, "+");
+          // preceded by - but that is the whole string
+          else if (/^-$/.test(prev)) this.text = prev.replace(/.$/, "");
+          else this.text = prev + "-";
+          break;
+        case ".":
+          // don't put two decimal points in a row
+          if (!/\.$/.test(prev)) this.text = prev + key;
+          break;
+        case "=":
+          if (/^.*\d+$/.test(prev)) {
+            this.text = String(evaluate(prev));
+          }
+          break;
+        default:
+          this.text = prev + key;
+      }
+    },
   },
 };
 </script>
