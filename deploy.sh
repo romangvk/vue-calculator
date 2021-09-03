c9747968a0d713f780b26c0e01dad9318c625df7
Roman Kasparian <roman.kasparian@gmail.com>
2021-09-03 10:59:25 -0400

    removed cname from deploy

--- deploy.sh
+++ deploy.sh
@@ -10,7 +10,7 @@ npm run build
 cd dist
 
 # if you are deploying to a custom domain
-echo 'https://romankasparian.dev/vue-calculator' > CNAME
+# echo 'https://romankasparian.dev/vue-calculator' > CNAME
 
 git init
 git add -A
